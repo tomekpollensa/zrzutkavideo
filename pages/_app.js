ee9ca84cb1080e03da2e96e11d187d5d41b95bdb
tomekpollensa <info.xvrtv@gmail.com>
2025-07-29 16:54:30 +0200

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,7 @@
-// pages/_app.js
 import '../styles/globals.css';
 
-export default function App({ Component, pageProps }) {
+function MyApp({ Component, pageProps }) {
   return <Component {...pageProps} />;
 }
+
+export default MyApp;
